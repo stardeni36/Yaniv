eadae538755a124baecda88b5f01846f5ab774c8
Eyal Seelig <eyal.seelig@gmail.com>
2021-01-23 17:43:49 +0200

    changed shuffle function name

--- yaniv_game.py
+++ yaniv_game.py
@@ -16,9 +16,9 @@ class PackOfCards:
         if cards:  # not None
             self.cards = cards
         if is_shuffle:
-            self.shuffle_pack()
+            self.shuffle()
 
-    def shuffle_pack(self):
+    def shuffle(self):
         shuffle(self.cards)
 
     def __str__(self):
@@ -118,7 +118,7 @@ class Game:
     def repopulate_deck(self):
         card_inds_to_move = range(1, len(self.stack.cards))  # TODO: make sure the indices are ok
         self.stack.distributed_by_indices(self.deck, card_inds_to_move)
-        self.deck.shuffle_pack()
+        self.deck.shuffle()
 
     def draw(self, player):
         response = player.choice_take_card()
